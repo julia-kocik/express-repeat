25270c53c3140a8b062d21632c32fe631a1824f9
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-05-01 17:30:40 +0200

    Add new routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const testimonialRoutes = require('./routes/testimonials.routes');
 const concertRoutes = require('./routes/concerts.routes');
 const seatRoutes = require('./routes/seats.routes');
@@ -9,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors())
 
-
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use('/api', testimonialRoutes);
 app.use('/api', concertRoutes);
 app.use('/api', seatRoutes);
@@ -18,6 +20,11 @@ app.use('/api', seatRoutes);
 app.use((req, res) => {
     res.status(404).send({ message: "Not found..." });
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
+  
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
